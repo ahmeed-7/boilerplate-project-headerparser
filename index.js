f3fb3a13f4eebd569edf64b089cbee2c6e48b992
ahmeed-7 <hkiriahmed120@gmail.com>
2025-12-19 14:33:13 +0100

    Add '/api/whoami' endpoint to return user info

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  var software=req.headers['user-agent'];
+  var language=req.headers['accept-language'];
+  var ipaddress=req.headers['x-forwarded-for'];
+  res.json({ipaddress:ipaddress,language:language,software:software});
 });
 
 // listen for requests :)
